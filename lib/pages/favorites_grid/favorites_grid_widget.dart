47f20da3489e8d3d10275ac7a1aac1098a7fe331
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-25 23:10:01 +0000

    Cleanup favorites grid

--- lib/pages/favorites_grid/favorites_grid_widget.dart
+++ lib/pages/favorites_grid/favorites_grid_widget.dart
@@ -283,24 +283,13 @@ class _FavoritesGridWidgetState extends State<FavoritesGridWidget> {
                                 itemBuilder: (context, allBooksIndex) {
                                   final allBooksItem = allBooks[allBooksIndex];
                                   return Padding(
-                                    padding: const EdgeInsetsDirectional.fromSTEB(
-                                        16.0, 8.0, 16.0, 0.0),
+                                    padding: const EdgeInsets.all(8.0),
                                     child: Container(
-                                      width: double.infinity,
+                                      width: 80.0,
                                       height: 140.0,
                                       decoration: BoxDecoration(
                                         color: FlutterFlowTheme.of(context)
                                             .secondaryBackground,
-                                        boxShadow: const [
-                                          BoxShadow(
-                                            blurRadius: 4.0,
-                                            color: Color(0x33000000),
-                                            offset: Offset(
-                                              0.0,
-                                              2.0,
-                                            ),
-                                          )
-                                        ],
                                         borderRadius:
                                             BorderRadius.circular(0.0),
                                       ),
@@ -325,17 +314,22 @@ class _FavoritesGridWidgetState extends State<FavoritesGridWidget> {
                                         },
                                         child: Stack(
                                           children: [
-                                            Hero(
-                                              tag: allBooksItem.coverPhoto,
-                                              transitionOnUserGestures: true,
-                                              child: ClipRRect(
-                                                borderRadius:
-                                                    BorderRadius.circular(0.0),
-                                                child: Image.network(
-                                                  allBooksItem.coverPhoto,
-                                                  width: double.infinity,
-                                                  height: 120.0,
-                                                  fit: BoxFit.contain,
+                                            Align(
+                                              alignment: const AlignmentDirectional(
+                                                  0.0, 0.0),
+                                              child: Hero(
+                                                tag: allBooksItem.coverPhoto,
+                                                transitionOnUserGestures: true,
+                                                child: ClipRRect(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          0.0),
+                                                  child: Image.network(
+                                                    allBooksItem.coverPhoto,
+                                                    width: 80.0,
+                                                    height: 140.0,
+                                                    fit: BoxFit.contain,
+                                                  ),
                                                 ),
                                               ),
                                             ),
@@ -430,24 +424,13 @@ class _FavoritesGridWidgetState extends State<FavoritesGridWidget> {
                                   final searchResultsItem =
                                       searchResults[searchResultsIndex];
                                   return Padding(
-                                    padding: const EdgeInsetsDirectional.fromSTEB(
-                                        16.0, 8.0, 16.0, 0.0),
+                                    padding: const EdgeInsets.all(8.0),
                                     child: Container(
-                                      width: double.infinity,
+                                      width: 80.0,
                                       height: 140.0,
                                       decoration: BoxDecoration(
                                         color: FlutterFlowTheme.of(context)
                                             .secondaryBackground,
-                                        boxShadow: const [
-                                          BoxShadow(
-                                            blurRadius: 4.0,
-                                            color: Color(0x33000000),
-                                            offset: Offset(
-                                              0.0,
-                                              2.0,
-                                            ),
-                                          )
-                                        ],
                                         borderRadius:
                                             BorderRadius.circular(0.0),
                                       ),
@@ -472,17 +455,24 @@ class _FavoritesGridWidgetState extends State<FavoritesGridWidget> {
                                         },
                                         child: Stack(
                                           children: [
-                                            Hero(
-                                              tag: searchResultsItem.coverPhoto,
-                                              transitionOnUserGestures: true,
-                                              child: ClipRRect(
-                                                borderRadius:
-                                                    BorderRadius.circular(0.0),
-                                                child: Image.network(
-                                                  searchResultsItem.coverPhoto,
-                                                  width: double.infinity,
-                                                  height: 120.0,
-                                                  fit: BoxFit.contain,
+                                            Align(
+                                              alignment: const AlignmentDirectional(
+                                                  0.0, 0.0),
+                                              child: Hero(
+                                                tag: searchResultsItem
+                                                    .coverPhoto,
+                                                transitionOnUserGestures: true,
+                                                child: ClipRRect(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          0.0),
+                                                  child: Image.network(
+                                                    searchResultsItem
+                                                        .coverPhoto,
+                                                    width: 80.0,
+                                                    height: 140.0,
+                                                    fit: BoxFit.contain,
+                                                  ),
                                                 ),
                                               ),
                                             ),
